2e283f0d609c14615bb78585e039628346687db0
ishasingh3 <138361249+ishasingh3@users.noreply.github.com>
2023-12-23 13:25:44 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treats all js code as newer version
+
+// alert("hello") //will give error as not using on browser (using nodejs)
+
+console.log(3 + 3); //make sure code is readable
+
+// different datatypes
+
+// number (upto 2^53)
+let age = 19
+
+// "bigint" to store big numbers
+
+// string (can use "" or '')
+let name = "isha"
+
+//boolean (either true or false)
+let access = "false"
+
+//null
+let salary = null //js can take variable value as null but the 'type' will show oject
+
+//undefined 
+let price;
+
+//symbol (unique)
+
+console.log(typeof salary); //output = 'object'
+console.log(typeof price);
